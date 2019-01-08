9661eb25c4d994954bf6a5ef5b9038515df3545a
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-01-09 01:33:13 +0700

    add swagger dependencies

--- demo/src/main/java/com/demo/config/UserPrincipal.java
+++ demo/src/main/java/com/demo/config/UserPrincipal.java
@@ -6,19 +6,19 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 
 public class UserPrincipal extends User {
-    private final Object user;
+    private final com.demo.entity.User user;
 
-    public UserPrincipal(Object user, Collection<? extends GrantedAuthority> authorities) {
-        super("user.getUsername()", "user.getPassword()", authorities);
+    public UserPrincipal(com.demo.entity.User user, Collection<? extends GrantedAuthority> authorities) {
+        super(user.getUsername(), user.getPassword(), authorities);
         this.user = user;
     }
 
-    public UserPrincipal(Object user, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+    public UserPrincipal(com.demo.entity.User user, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
         super("user.getUsername()", "user.getPassword()", enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
         this.user = user;
     }
 
-    public Object getUser() {
+    public com.demo.entity.User getUser() {
         return this.user;
     }
 }
