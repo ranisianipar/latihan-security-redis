868ab1aafb43d359378afcbfa1cfcd28b7afb527
natval98 <natasha16@mhs.if.its.ac.id>
2019-01-09 07:24:52 +0700

    add cache in controller

--- demo/src/main/java/com/demo/controller/AdminController.java
+++ demo/src/main/java/com/demo/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.demo.controller;
 import com.demo.entity.User;
 import com.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,16 +27,19 @@ public class AdminController {
     }
 
     @GetMapping("/user/{id}")
+    @Cacheable(key = "#id", value = "user")
     public User readUser(@PathVariable("id") String id){
         return userService.readUser(id);
     }
 
     @PutMapping("/user/{id}")
+    @CacheEvict(key = "#user.id", value = "user")
     public User updateUser(@PathVariable("id") String id, @RequestBody User user){
         return userService.updateUser(id, user);
     }
 
     @DeleteMapping("/user/{id}")
+    @CacheEvict(key = "#user.id", value = "user")
     public String deleteUser(@PathVariable("id") String id){
         userService.deleteUser(id);
         return "Success to be deleted!";
